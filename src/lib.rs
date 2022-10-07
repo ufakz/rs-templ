d69adc4aac83c19e41f4722372443c839d8669fd
Farouq Umar <ufakz99@gmail.com>
2022-10-07 14:49:53 +0100

    implement render function

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,57 @@ impl fmt::Display for Data {
     }
 }
 
+fn render(mut template: String, mut data: HashMap<&str, Data>) -> String {
+    let print_regex = Regex::new(r"\{\{(.*?)\}\}").unwrap();
+    template = print_regex
+        .replace_all(&template, |caps: &Captures| {
+            let key = caps.get(1).unwrap().as_str().trim();
+            data[key].to_string()
+        })
+        .to_string();
+
+    let repeat_regex =
+        Regex::new(r"\{% repeat (\d*?) times %\}((.|\n)*?)\{% endrepeat %\}").unwrap();
+    template = repeat_regex
+        .replace_all(&template, |caps: &Captures| {
+            // Extract the number of times to repeat the code
+            let times = caps.get(1).unwrap().as_str().trim();
+            // Parse the code block to be repeated
+            let code = caps.get(2).unwrap().as_str().trim();
+            // Repeat the code `times` number of times
+            code.repeat(times.parse::<usize>().unwrap())
+        })
+        .to_string();
+
+    let if_else_regex = Regex::new(
+        r"\{% if (.*?) %\}((.|\n)*?)(\{% else %\}((.|\n)*?)\{% endif %\}|\{% endif %\})",
+    )
+    .unwrap();
+    template = if_else_regex
+        .replace_all(&template, |caps: &Captures| {
+            // Extract the name of the bool being tested
+            let key = caps.get(1).unwrap().as_str().trim();
+            // Parse the 'if' and (optional) 'else' code blocks
+            let if_code = caps.get(2).unwrap().as_str().trim();
+            let else_code = caps.get(5).map_or("", |m| m.as_str()).trim();
+            // Find the corresponding key in the Data and return the value
+            if let Data::Boolean(exp) = data[key] {
+                if exp {
+                    if_code.to_string()
+                } else {
+                    else_code.to_string()
+                }
+            } else {
+                "ERROR PARSING KEY".to_string()
+            }
+        })
+        .to_string();
+
+    template = template.replace("{#", "<!--").replace("#}", "-->");
+
+    template
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
