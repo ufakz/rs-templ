0b39d55315fb5566b2a7571ea5a06d79eeb36a5c
Farouq Umar <ufakz99@gmail.com>
2022-10-07 14:42:08 +0100

    implement Display trait for Data enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use regex::{Captures, Regex};
+use std::{collections::HashMap, fmt};
+
+enum Data {
+    Number(i32),
+    Boolean(bool),
+    Text(String),
+}
+
+impl fmt::Display for Data {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Text(x) => write!(f, "{}", x),
+            Self::Number(x) => write!(f, "{}", x),
+            Self::Boolean(x) => write!(f, "{}", x),
+        }
+    }
 }
 
 #[cfg(test)]
